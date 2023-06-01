82ed3f27e5d9767d512b687342d92112e3458024
Roberto Piombi <rpiombi@bna.com.ar>
2023-06-01 12:59:11 -0300

    ELK funcionando con Api Clientes

--- ejercicio01/src/main/java/ar/com/bna/ejercicio01/services/ClienteService.java
+++ ejercicio01/src/main/java/ar/com/bna/ejercicio01/services/ClienteService.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Service;
 import ar.com.bna.ejercicio01.kafka.StringPublisher;
 import ar.com.bna.ejercicio01.model.Cliente;
 import ar.com.bna.ejercicio01.repository.ClienteRepository;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service("ClienteService")
 public class ClienteService {
 
@@ -20,27 +22,37 @@ public class ClienteService {
 	
 	public List<Cliente> list() {
 		stringPublisher.produce("list");
-		return clienteRepository.findAll();
+		List<Cliente> lista = clienteRepository.findAll();
+		log.info("list -> " + lista.toString());
+		return lista;
 	}
 
 	public Optional<Cliente> getClienteById(int id) {
 		stringPublisher.produce("getClienteById " + id);
-		return clienteRepository.findById(id);
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		log.info("getClienteById -> " + cliente.toString());
+		return cliente;
 	}
 
 	public Optional<Cliente> getClienteByCuil(String cuil) {
 		stringPublisher.produce("getClienteByCuil " + cuil);
-		return clienteRepository.findByCuil(cuil);
+		Optional<Cliente> cliente = clienteRepository.findByCuil(cuil);
+		log.info("getClienteByCuil -> " + cliente.toString());
+		return cliente;
 	}
 
 	public Cliente addCliente(Cliente cliente) {
 		stringPublisher.produce("addCliente " + cliente.toString());
-		return clienteRepository.save(cliente);
+		Cliente resultado = clienteRepository.save(cliente);
+		log.info("addCliente -> " + resultado.toString());
+		return resultado;
 	}
 
 	public Cliente updateCliente(Cliente cliente) {
 		stringPublisher.produce("updateCliente " + cliente.toString());
 		Optional<Cliente> clienteEncontrado = clienteRepository.findById(cliente.getId());
-		return clienteEncontrado.isPresent() ? clienteRepository.save(cliente): null ;
+		Cliente resultado = clienteEncontrado.isPresent() ? clienteRepository.save(cliente): null ;
+		log.info("updateCliente -> " + resultado.toString());
+		return resultado;
 	}
 }
